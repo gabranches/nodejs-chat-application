801824ed7b47ff6e39ac0e16e5f1af08aeba264b
Gustavo Abranches <gabranches@gmail.com>
2015-10-14 22:21:32 -0400

    refactoring, fixed auto-scroll bug

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,16 +1,43 @@
+function printMessage(msg, own) {
+    // Print message to chat box
+    if (own) {
+        $('#chatbox').append($('<div class="own message">').text('Anonymous: ' + msg))
+    } else {
+        $('#chatbox').append($('<div class="message">').text('Anonymous: ' + msg))
+    }
+    $("#chatbox").prop({ scrollTop: $("#chatbox").prop("scrollHeight") })
+}
+
+function flashTitle(title) {
+  // Makes page title flash on tap if it is out of focus
+  if (document.hasFocus()){
+    document.title = title
+    clearInterval(timer)
+    timerOn = false
+  } else {
+    if (timerOn == false) {
+      timer = setInterval(function(){ 
+        document.title = document.title == title ? 'New Message!' : title
+      }, 1000);
+      timerOn = true
+    }
+  }
+}
+
 function resize() {
+    // Resize chat window to fit screen properly
     var height = $(window).height();
     $('#chatbox').css('height', height - 130 + 'px');
 }
 
 $(window).focus(function () {
-    flashTitle()
+    flashTitle(client.room)
     clearInterval(timer)
     timerOn = false
 })
 
 $(document).on('focus', function () {
-    flashTitle()
+    flashTitle(client.room)
     clearInterval(timer)
     timerOn = false
 })
@@ -20,6 +47,7 @@ $(window).resize(function() {
 });
 
 window.setInterval( function () {
+    // Update room info every 10 seconds
     if (client) {
         socket.emit('info', client)
     }
